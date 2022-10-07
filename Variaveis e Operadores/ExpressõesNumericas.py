9cd750268f5ca26a66bf3b27ea8acd5d5d0ee3ae
Hugox96 <alveshugo2906@gmail.com>
2022-10-07 16:17:15 -0300

    EstudandoExpressoes

--- Variaveis e Operadores/ExpressõesNumericas.py
+++ Variaveis e Operadores/ExpressõesNumericas.py
@@ -10,3 +10,15 @@ print(expressao)
 
 # 1º Operador é a potência(**), 2º multplicação(*) e divisão(%) (inteira / ou decimal //) e 3º soma(+) e subtração (-)
 
+# Ex1: Qual é o valor da expressão abaixo? 
+expressão1 = 16 - 2 * 5 / 3 + 1
+
+Operação_1 =  (2 * 5)
+Operação_2 =  (Operação_1 // 3)
+Operação_3 =  (16 - Operação_2)
+Operação_4 = Operação_3 + 1
+print(Operação_4)
+
+
+print(expressão1)
+
