7fb2d9f0dc6fc5d55dda448a8689a41c561c5574
Hugox96 <alveshugo2906@gmail.com>
2022-10-07 15:28:39 -0300

    estudando variaveis

---
+++ Variaveis e Operadores/VariaveisBásicas.py
@@ -0,0 +1,19 @@
+# Variaveis são como armazenamos os dados em python, eles podem ser do tipo float, int, string e caracteres
+
+# Declarando variaveis inteiras
+a = 10
+b = 30 
+c = 20 
+print(a + b + c)
+
+# Declarando Strings
+nome = "hugo"
+print("Olá",  nome)
+
+# Declarando floats
+d = 9.8
+print(d + a)
+
+# criando uma variável para operações
+soma = d + a
+print("na variavel nova",soma)
