4b86b8b9679ee733ce039ca9a7b22a709db39306
Hugox96 <alveshugo2906@gmail.com>
2022-10-07 15:34:30 -0300

    estudando operadores

---
+++ Variaveis e Operadores/OperadoresBásicos.py
@@ -0,0 +1,17 @@
+# Soma e subtração
+print(10 + 2 - 7)
+
+# Potência: 2 ^ 4
+print(2 ** 4)
+
+# Divisão de ponto flutuante
+print(20 / 6)
+
+# Divisão inteira, sem considerar o resto
+print(20 // 6)
+
+# Resto da divisão
+print(20 % 6) 
+
+#Raiz de um numero
+print(4**1/2)
