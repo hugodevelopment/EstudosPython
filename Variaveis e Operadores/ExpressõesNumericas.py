ad854e441783ef34c849ffa3702b5b1d69f80488
Hugox96 <alveshugo2906@gmail.com>
2022-10-07 15:52:48 -0300

    estudandoOperadores

--- Variaveis e Operadores/ExpressõesNumericas.py
+++ Variaveis e Operadores/ExpressõesNumericas.py
@@ -5,3 +5,8 @@ expressao = (1 * -2 * 3) * (-4 * 5 * -6)
 print(expressao)
 
 # Regras de Precedência
+# Quando criamos uma expressão em Python, existe em uma ordem em que as subexpressões são avaliadas. 
+# Essa ordem é determinada por algo que chamamos de precedência de operadores.
+
+# 1º Operador é a potência(**), 2º multplicação(*) e divisão(%) (inteira / ou decimal //) e 3º soma(+) e subtração (-)
+
