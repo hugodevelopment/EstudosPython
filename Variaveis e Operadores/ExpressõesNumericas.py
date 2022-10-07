e8a171f2062eec1af7e9ca67c07f2d3489f63d7d
Hugox96 <alveshugo2906@gmail.com>
2022-10-07 15:46:45 -0300

    estudandoExpressões

---
+++ Variaveis e Operadores/ExpressõesNumericas.py
@@ -0,0 +1,7 @@
+# Expressões matemáticas em Python podem obedecem as 
+# mesmas regras de sinal com as quais estamos acostumados:
+
+expressao = (1 * -2 * 3) * (-4 * 5 * -6)
+print(expressao)
+
+# Regras de Precedência
