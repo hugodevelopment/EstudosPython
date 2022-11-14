c415ffb7469cd8510224bf97ea630de809499e08
Hugox96 <alveshugo2906@gmail.com>
2022-11-14 15:42:33 -0300

    estudado_range

---
+++ Loops e Listas/For/For(I_in_Range).py
@@ -0,0 +1,23 @@
+#  for loops repeat a portion of code for a set of values.
+#  for loops work slightly differently than they do in languages such as JavaScript or C.
+#  A for loop sets the iterator variable to each value in a provided list, array, 
+# or string and repeats the code in the body of the for loop for each value of the iterator variable.
+
+# Ex 1
+lista = [1,3,4,5,6,7,8]
+for i in lista:
+    print("printando cada valor da lista",i)
+
+# We can include more complex logic in the body of a for loop as well. 
+# Ex 2:
+for i in lista:
+ x = i**2 - (i-1)*(i+1)
+#  print(x, end= ", ")
+
+#  Criando funções para realizar o mesmo calculo
+def sempre_1(x):
+    return x**2 - (x-1)*(x+1)
+
+for i in lista:
+    sempre_1(i)
+    print(sempre_1(i))
---
+++ Loops e Listas/For/For(PercorrendoLista).py
@@ -0,0 +1,26 @@
+
+# Criando uma função para verificar o a situação do aluno
+def situação(value,x):
+   if (value > 7):
+      print("Aprovado com ", value, x)
+   else:
+      print("Reprovado com ",value, x)   
+
+alunos = ["Hugo", "Lucas", "Marcio", "Adonai"]
+Notas =  [9,8,7,9]
+Notas2 = [4,6,5,6]
+
+media = 0
+for i in range(len(alunos)):
+   # print(alunos)
+   # A variável soma pega o primeiro valor de notas e notas2 e faz a soma, por exemplo, 9+4, 6+8
+   soma = Notas[i] + Notas2[i]
+   # print(soma)
+   media = soma/2
+   situação(media, alunos[i]) 
+
+
+
+for i in alunos:
+    soma = Notas + Notas2
+    print(soma)
