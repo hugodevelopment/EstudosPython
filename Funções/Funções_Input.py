2bb15333fa2442d7a18a13b17ec899eed697f0a5
Hugox96 <alveshugo2906@gmail.com>
2022-12-10 12:25:57 -0300

    ok

--- Funções/Funções_Input.py
+++ Funções/Funções_Input.py
@@ -1,13 +1,37 @@
 
-# Minha 
-
+# Minha ideia é aprender a utilizar inputs em funções para o usuário interagir, assim podemos criar até um projeto de calculadora
 def soma():
     x = int(input("valor de x"))
     y = int(input("valor de y"))
-    return x+y
+    soma = x+y
+    return soma
+
+def subtração():
+    x = int(input("valor de x"))
+    y = int(input("valor de y"))
+    sub = x-y
+    return sub
 
 
-i=0
-while(i < 2):
-    print(soma())  
-    i+=1  
+# i=0
+# while(i < 1):
+#     print("Escola uma opeação: 1 para soma e 2 para sub:")
+#     opção = int(input("qual operação:"))
+#     if opção == 1:
+#         print("a soma de x e y é:", soma()) 
+#     else:
+#         print("a sub é", subtração())    
+#     i+=1  
+
+while True:
+    cont = 'S'
+    print("Escola uma opeação: 1 para soma e 2 para sub:")
+    opção = int(input("qual operação:"))
+    if opção == 1:
+        print("a soma de x e y é:", soma()) 
+    else:
+        print("a sub é", subtração())
+    cont = input("Deseja continuar? S|N") 
+    if cont != 'S':
+        break       
+
