8626c1fd366326901667100d600c3650cbd0bcec
Hugox96 <alveshugo2906@gmail.com>
2022-12-19 20:25:18 -0300

    estudando_slicing

--- Loops e Listas/Lista/Lista_Slicing.py
+++ Loops e Listas/Lista/Lista_Slicing.py
@@ -11,8 +11,18 @@
 # 4) If step is not given, steps will increase by 1
 
 
+# Exemplo 1
+
 lista = [1,5,6,7,8,9]
 # neste caso a variavel a receberá os numeros que correspondem a posição 1 e 3 da lista, já que o ultimo número(4) não conta
 a= lista[1:5]
 print(a)
 print("4º posição da lista:", lista[4])
+
+
+#Exemplo 2 com a função range
+lista = list(range(1,15,2))  # a função range cria uma lista que vai de 1 a 15(não-incluindo) de 2 em 2 números
+print(lista)
+a = lista[2:5]
+print(a)
+
