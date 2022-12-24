f73d0e757f49f8c03bbf4f8ecb37be76282ca4de
Hugox96 <alveshugo2906@gmail.com>
2022-12-23 23:03:53 -0300

    ok

--- Dicionários/Dicionario1.py
+++ Dicionários/Dicionario1.py
@@ -63,3 +63,20 @@ data = {
 
 a = input()
 print(data.get(a, "Not found"))
+
+
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+name = input("John")
+
+for x in contacts:
+    if name in x:
+        print(str(x[0]) + "is" + str(x[1]))
+        break
+else:
+    print("vai a merda")
