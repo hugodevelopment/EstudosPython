d97cdfa4fa4f904d8e07252d41b567fa5bda084f
Hugox96 <alveshugo2906@gmail.com>
2023-01-06 12:32:48 -0300

    revisando

--- Loops e Listas/Lista/Ex2(ListaAppend).py
+++ Loops e Listas/Lista/Ex2(ListaAppend).py
@@ -1,14 +1,37 @@
 
-def dobro(lista):
+def dobro(x):
     # Criando uma lista vazia para receber o resultado
     resultado = []
     # Loop para calcular a lista de numeros
-    for x in lista:
+    for i in x:
         # Criando o calculo para elevar os números ao quadrado
         resultado.append(x**2)
     return resultado    
 
 lista = [1, 4, 9, 20]
 print(dobro(lista))
+
+"----------------------------------------------------------------------------------------------------------------------------------"
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango", "uvu"]
+
+nova_lista = []
+def novalista(x):
+  if "a" in x:
+      nova_lista.append(x)
+  return nova_lista
+    	
+for i in fruits:
+	novalista(i)
+print("1",nova_lista)    
+
+   
+    
+    
+	
+
+    	
+
+
 
 
