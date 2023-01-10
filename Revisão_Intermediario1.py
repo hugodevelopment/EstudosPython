f5c3fd5a80e60e566be7e73cdd059548831f197e
Hugox96 <alveshugo2906@gmail.com>
2023-01-09 23:06:19 -0300

    revisando

--- Revisão_Intermediario1.py
+++ Revisão_Intermediario1.py
@@ -20,19 +20,28 @@ lista = [i *10  for i in range(5,9)]
 print(lista)
 
 
-# nome = "amanda"
-# count = 0
-# for i in nome:
-#     if i == nome[0]:
-#         count += 1
-# print(count)
 
-
-def count_letters(word, to_find):
+def contando_letras(nome, letra):
     count = 0
-    for char in word:
-        if char == to_find:
-            count += 1
+    for i in nome:
+        if i == letra:
+            count += 1            
+    print(letra," tem ", count)       
     return count
 
-print(count_letters("amanda","a"))
+print(contando_letras("hugoo", "o"))    
+
+
+# Treinando iteração com dicionarios: Minha ideia é iterar automaticamente dicionarios para adicionar valores
+
+# 1º Criando a função para receber nome e nota
+def dicio(keys, i, values):
+    alunos = {}
+    alunos[keys[i]] = values[i]
+    return alunos
+
+# 2º criando um for para adicionar os valores:
+for i in range(2):
+    nome = input("insira um nome:")
+    nota = input("Insira a nota")
+    
