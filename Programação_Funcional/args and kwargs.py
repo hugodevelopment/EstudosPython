6d181e288b473a9f424589edd696e67a8097166d
Hugox96 <alveshugo2906@gmail.com>
2023-01-30 16:23:47 -0300

    estudando_args

--- Programação_Funcional/args and kwargs.py
+++ Programação_Funcional/args and kwargs.py
@@ -3,6 +3,7 @@
 
 # Nesta função soma, utilizei o *args para enviar mais parametros de uma vez a função
 def soma(*x):
+    print(x)
     total = 0 
     for i in x:
         total += i
@@ -15,3 +16,34 @@ print(soma(2,3,4))
 
 # **kwargs allows us to pass a variable number of keyword arguments to a Python function. In the function, we use the double-asterisk (**) 
 # before the parameter name to denote this type of argument.
+
+def total_fruits(**kwargs):
+    # Kwargs retorna um dicionário
+    print(kwargs, type(kwargs))
+total_fruits(banana=5, mango=7, apple=8)
+
+
+# Melhorando a função que recebe o dicionário com kwargs
+def total_fruits(**fruits):
+    total = 0
+    for i in fruits.values():
+        if i > 10:
+            print("Muito caro hein", i)
+        total += i       
+    return total 
+
+print(total_fruits(laranja =10, abacaxi=13, limão=5))       
+
+
+# def my_min(*x):
+#     menor = 500
+#     print(x)
+#     for i in x:
+#         print(i)
+#         if i < menor:
+#             menor = i
+#             return menor
+#         else:
+#             return i
+
+# print(my_min(8, 13, 4, 42, 120, 7))
