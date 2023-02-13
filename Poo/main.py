f27dc2f632eb2d75846dc32bf595cbfa76ab47a2
Hugox96 <alveshugo2906@gmail.com>
2023-02-13 18:10:10 -0300

    Estudando_Classes

---
+++ Poo/main.py
@@ -0,0 +1,26 @@
+from pessoa import Pessoa
+
+# Hhama-se instância de uma classe, 
+# Um objeto cujo comportamento e estado são definidos pela classe. 
+# O uso da palavra "Instância" pode ter sido inspirado no inglês; 
+# tal palavra significa "caso" ou "exemplo" em inglês
+
+
+# vamos instanciar a classe pessoa, criando objetos
+# pessoa1= Pessoa()
+# pessoa2= Pessoa()
+
+# Criando atributos para as pessoas de uma maneira menos eficaz
+# pessoa1.nome = "Hugo"
+# pessoa2.nome = "Lucas"
+# print(pessoa1.nome)
+# print(pessoa2.nome)
+
+# Agora vamos criar de uma maneira melhor, utilizando a classe Pessoas
+pessoa1 = Pessoa("Hugo", 26)
+pessoa2 = Pessoa("Marcio",25)
+pessoa1.comer("Maça")
+pessoa1.comer("Pera")
+pessoa1.parar_de_comer()
+pessoa1.parar_de_comer()
+pessoa1.comer()
---
+++ Poo/pessoa.py
@@ -0,0 +1,25 @@
+class Pessoa:
+    # Criando um metodo da classe Pessoa, metodos são funções criadas dentro de uma classe
+    def falar(self): # O self é utilizado para referenciar o metodo a uma instancia, sendo cada instancia única
+        print("Estou falando com vc")
+    
+    # Criando um método inicial para nossa classe
+    def __init__(self, nome, idade, comendo= False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def comer(self, voce):
+        if self.comendo:
+            print(f'{self.nome}, já está comendo')
+            return
+        print(f'{self.nome}, Está comendo o ' , f'{voce}')
+        self.comendo = True 
+
+    def parar_de_comer(self):
+        if self.comendo == True:
+            print("Vou parar de comer ")   
+        else:        
+            print("Termimando...")
+        self.comendo = False
