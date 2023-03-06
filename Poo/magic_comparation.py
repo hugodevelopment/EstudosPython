8c4ce79dd431502d19ec6c76f4a289a629d53120
Hugox96 <alveshugo2906@gmail.com>
2023-03-06 16:13:57 -0300

    revisando

--- Poo/magic_comparation.py
+++ Poo/magic_comparation.py
@@ -1,4 +1,4 @@
-class Maior:
+class Comparações:
     def __init__ (self, a):
         self.a = a
 
@@ -8,10 +8,25 @@ class Maior:
             return True
         else:
             return False
+        
 
-num1 = Maior(400)
-num2 = Maior(300)
+    def __it__(self, other):
+        if (self.a < other.a):
+            return "É menor"
+        else:
+            return "É maior" 
+
+    def __eq__(self, other):
+        if (self.a == other.a):
+            return "É igual"
+        else:
+            return "É diferente"      
+
+num1 = Comparações(300)
+num2 = Comparações(300)
 
 print(num1.__gt__(num2))
+print(num1.__it__(num2))
+print(num1.__eq__(num2))
 
 
--- Poo/vector.py
+++ Poo/vector.py
@@ -5,10 +5,17 @@ class vector:
 
     def __add__(self, other):
         return self.x + other.x, self.y + other.y
+    
+    def __eq__(self, other):
+        if (self.x == other.x):
+            return "Iguais"
+        else:
+            return "diferentes"
 
-vetor1 = vector(2,3)
-vetor2 = vector(4,4)
+vetor1 = vector(3,3)
+vetor2 = vector(2,4)
 
 print("normal", vetor1 + vetor2)
 print("com magic methods", vetor1.__add__(vetor2))
+print(vetor1 == vetor2)
 
