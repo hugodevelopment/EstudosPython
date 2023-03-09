1b59559cae4575599b4cddf087e7c7b0d5bca369
Hugox96 <alveshugo2906@gmail.com>
2023-03-09 11:13:15 -0300

    ok

--- Poo/Magic Methods.py
+++ Poo/Magic Methods.py
@@ -11,21 +11,24 @@ print("Hugo" + " Alves")
 class Soma:
     def __init__(self,x):
         self.x = x
-
+    # metodo add para realizar a soma 
     def __add__(self, y):
         return(self.x + y.x) 
 
-
+# instanciando a classe Soma
 num1 = Soma(3)
 num2 = Soma(4)
+num3 = Soma(5)
 
 palavra1 = Soma("hugo")
 palavra2 = Soma("lucas")
 
+# Somando com o metodo convencional
 result = palavra1 + palavra2
 print("soma de palavras", result)
 
 print("Soma convencional", num1 + num2)
+# Somando com o magic methods
 print("Soma com magic methods", num1.__add__(num2))
 print("Soma de palavras com magic methods", palavra1.__add__(palavra2))
 
--- Poo/vector.py
+++ Poo/vector.py
@@ -19,3 +19,36 @@ print("normal", vetor1 + vetor2)
 print("com magic methods", vetor1.__add__(vetor2))
 print(vetor1 == vetor2)
 
+
+class Shape: 
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def area(self):
+        return self.width*self.height
+
+    #your code goes here
+    def __add__(self, other):
+        return Shape(self.width + other.width, self.height + other.height)
+
+    def __gt__(self,other):
+        if (self.area() > other.area()):
+            return True
+        else:
+            return False         
+    
+
+w1 = int(input())
+h1 = int(input())
+w2 = int(input())
+h2 = int(input())
+
+s1 = Shape(w1, h1)
+s2 = Shape(w2, h2)
+result = s1 + s2
+
+print(result.area())
+print(s1 > s2)
+
+   
