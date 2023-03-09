bc315d2b2fd4597db710754207fe541017feded2
Hugox96 <alveshugo2906@gmail.com>
2023-03-09 11:50:30 -0300

    ok

--- Poo/encapsulamento.py
+++ Poo/encapsulamento.py
@@ -12,5 +12,79 @@ class funcionario:
         self.cargo = cargo
         self.valor_hora_trabalhada = valor_hora_trabalhada
         # Posso criar variaves mesmo fora do init
-        self.valor_hora_trabalhada = 0
+        self.horas_trabalhadas = 0
         self.salario = 0
+
+    def horas_trabalhos(self):
+        self.horas_trabalhadas =+ 1
+
+    def calcular_salário(self):
+        self.salario = self.horas_trabalhadas * self.valor_hora_trabalhada        
+
+
+# O problema desta classe é que ela pode alterar o salario de um cliente de modo arbitrario, 
+# sem precisar utilizar os metodos criadas na classe
+
+funcionario1 = funcionario("Hugo", "estudante" , 3)
+# coloquei 1000 reais de maluco
+salario = funcionario1.salario = 1000
+
+
+# Em Python, existe uma convenção de que dados ou métodos cujo nome começa com 
+# _ (dois _underscores) não deveriam ser acessados fora da classe
+class funcionario:
+    def __init__(self, nome, cargo, valor_hora_trabalhada):
+        self.nome = nome
+        self.cargo = cargo
+        self.valor_hora_trabalhada = valor_hora_trabalhada
+        # Posso criar variaves mesmo fora do init
+        self.__horas_trabalhadas = 0
+        self.__salario  = 0
+
+    def horas_trabalhos(self):
+        self.horas_trabalhadas =+ 1
+
+    def calcula_salario(self):
+        self.__salario = self.__horas_trabalhadas * self.valor_hora_trabalhada
+
+# Isso já é um avanço, pois sinaliza a clientes da classe que as variáveis horas_trabalhadas e 
+# salario não devem ser alteradas arbritrariamente.
+
+funcionario1 = funcionario("Hugo", "estudante" , 3)
+# Ainda é possivel alterar, mesmo com as indicações
+funcionario1.__salario = 2000
+print(funcionario1.__salario)
+
+
+# Outra classe agora com encapsulamento
+class Funcionario:
+    def __init__(self, nome, cargo, valor_hora_trabalhada):
+        self.nome = nome
+        self.cargo = cargo
+        self.valor_hora_trabalhada = valor_hora_trabalhada
+        self.__salario = 0
+        self.__horas_trabalhadas = 0
+
+    @property
+    def salario(self): 
+        return self.__salario
+    
+    # Caso o usario tente alterar o salario não será possível
+    @salario.setter
+    def salario(self, novo_salario): 
+        raise ValueError("Impossivel alterar salario diretamente. Use a funcao calcula_salario().")
+
+    def registra_hora_trabalhada(self):
+        self.__horas_trabalhadas += 1
+
+    def calcula_salario(self):
+        self.__salario = self.__horas_trabalhadas * self.valor_hora_trabalhada
+
+
+funcionario1 = Funcionario("Hugo", "estagio" , 3)
+# Agora não será mais possível alterar
+funcionario1.salario = 3000
+print(funcionario1.salario)       
+funcionario1.registra_hora_trabalhada()
+funcionario1.calcula_salario()
+      
