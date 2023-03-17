658071a0d967c4adad1f178a50bb14ae0373fecb
Hugox96 <alveshugo2906@gmail.com>
2023-03-17 10:32:56 -0300

    ok

--- Execptios/Quiz.py
+++ Execptios/Quiz.py
@@ -11,12 +11,43 @@ finally:
 print()
 
 
+
 x = 3
 try: 
   x+="2"
 except:
   x+=2
+  print(x)
 else:
+  print("oi")
   x+=4
 finally:
   print(x)
+
+
+print()
+
+x = 10
+try:
+  x+=3
+  raise ValueError
+except NameError:
+  x+=1
+except ValueError:
+  x+=1
+else:
+  x+=1
+finally:
+  x+=1
+
+print(x)
+
+
+try:
+    name = input()
+    if len(name) < 4:
+        raise ValueError
+except:
+    print("Invalid Name")
+else:
+    print("Account Created") 
