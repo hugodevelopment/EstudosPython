61f6e71c18ff5df0672ee7a59ec2c92c4cab9abe
Hugox96 <alveshugo2906@gmail.com>
2023-04-18 23:40:50 -0300

    treinando funções

--- Loops e Listas/While/Ex 4 (Numero_Sorteado).py
+++ Loops e Listas/While/Ex 4 (Numero_Sorteado).py
@@ -4,16 +4,19 @@
 import random
 
 numero_magico = random.randint(0,100)
-# print(numero_magico)
+print(numero_magico)
+
+def descobrindo_numero(x):
+    if x == numero_magico:
+        print("Você acertou, parabéns")
+    else:
+        print("Voce não acertou dessa vez")
+     
 
 # Utilizando o while true para o usuario escolher continuar jogando
 while True:
         numero_user = int(input("Digite um número para brincar"))
-        if numero_user == numero_magico:
-            print("Você acertou, parabéns")
-            break
-        else:
-            print("Voce não acertou dessa vez")
+        descobrindo_numero(numero_user)
         opcao = int(input("Digite 1 para sair ou qualquer para continuar"))
         if opcao == 1:
             print("O numero magico era:", numero_magico)
