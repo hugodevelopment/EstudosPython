0a0a9cb6a3137205ad362de29493e7e6513cea84
Hugox96 <alveshugo2906@gmail.com>
2023-04-24 23:17:43 -0300

    inversa

--- Matrizes/matrizes2.py
+++ Matrizes/matrizes2.py
@@ -1,14 +1,25 @@
-matriz = [ [0 for i in range(2)] for j in range(4)]
+matriz = [ [0 for i in range(2)] for j in range(2)]
+matriz_inversa = [ [0 for i in range(2)] for j in range(2)]
 # count=0
 
-for linha in range(4):
-    for coluna in range(4):
+for linha in range(2):
+    for coluna in range(2):
         print("linha", linha)
         print("coluna", coluna)
         matriz[linha][coluna] = int(input("insira um numero"))
+        matriz_inversa[0][0] = matriz[1][1]
         # count += 1
     
-for linha in range(4):
-    for coluna in range(3):
+for linha in range(2):
+    for coluna in range(2):
         print("%5d" % matriz[linha][coluna], end='')
     print()
+
+print("Calculando inversa")
+
+for linha in range(2):
+    for coluna in range(2):
+        print("%5d" % matriz_inversa[linha][coluna], end='')    
+    print()    
+
+   
